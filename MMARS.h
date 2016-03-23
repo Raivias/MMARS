779ff72c7fec9bca82d6d5f65171b68016ce9499
raivias <raivias@gmail.com>
2016-03-23 12:15:53 -0400

    Files just to include mobility and perception libraries. Not currently working

---
+++ MMARS.h
@@ -0,0 +1,16 @@
+#ifndef MMARS_H
+#define MMARS_H
+
+#if (ARDUINO >= 100)
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+#include <Wire.h>
+
+#include "Perceptions/Perception.h"
+#include "Mobility/Mobility.h"
+
+
+#endif 
+//MMARS_H
